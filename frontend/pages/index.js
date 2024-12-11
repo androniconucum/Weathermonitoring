6000a5dc1bf49d58e04ce5a6fc1c4a91be373acf
Andronico Nucum <nicounwntdmain@gmail.com>
2024-12-12 02:34:29 +0800

    dfvdfvsdfv

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -121,7 +121,7 @@ export default function SignIn() {
 
         <div className="mt-6 text-center">
           <p className="text-sm text-gray-600">
-            Don't have an account? 
+            Dont have an account? 
             <button
               onClick={() => router.push('/signup')}
               className="font-medium text-blue-600 hover:text-blue-500"
